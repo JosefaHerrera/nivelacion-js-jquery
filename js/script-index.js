31ea76fb2d9ccc7c9326b025a99b8e12fa8b4d46
JosefaHerrera <josefa@linuxero.cl>
2017-07-10 10:35:24 -0400

    //jQuery hide() Method , {.hide()} //El metodo hide() nos permite ocultar algun elemento.

--- js/script-index.js
+++ js/script-index.js
@@ -1,9 +1,19 @@
+alert("pollo");
 $(document).ready( function(){
 
 	//La variable "recipesArray" esta declarada en el archivo "data/recipes.js"
 	renderHighlightedRecipes(recipesArray);
 
 });
+
+
+//jQuery hide() Method , {$(selector).hide()} 
+//El metodo hide() nos permite ocultar algun elemento.
+$("#hide").ready(function(){
+        $(".js-back").hide();
+    });
+
+
 
 
 /*
---
+++ js/script-recipe.js
@@ -0,0 +1,5 @@
+//jQuery hide() Method , {$(selector).hide()}
+//El metodo hide() nos permite ocultar algun elemento.
+$("#hide").ready(function(){
+        $(".js-menu").hide();
+    });
