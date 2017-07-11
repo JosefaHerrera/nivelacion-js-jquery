a1a543c7df852bc81822c86ac2976038b9138ec5
JosefaHerrera <josefa@linuxero.cl>
2017-07-10 20:17:06 -0400

    se realiza la llamada de la clase especifica, ocupamos el metodo append, y en la funcion renderActivities se llama la funcion renderActivity para que imprima en ese sector

--- js/script-index.js
+++ js/script-index.js
@@ -106,7 +106,8 @@ function renderActivities(activities) {
 * Función que se encarga de pintar una actividad
 * Aqui se tiene que crear el HTML que esta en el
 * archivo "templates/templates-activity.html"
-* se realiza una llamada de clase y ocupando el metodo append(); se cambia el nombre del parametro recipe a activities
+* se realiza una llamada de clase y ocupando el metodo append();
+
 */
 function renderActivity(recipe) {
 		$(".list-activities").append('<a href="#" class="item-activity"><span class="attribution"><span class="avatar"><img src="'+recipe.userAvatar+'" class="image-avatar"></span><span class="meta"><span class="author">'+recipe.userName+'</span> made <span class="recipe">'+recipe.recipeName+'</span>:'+recipe.text+'<span class="location">&mdash;'+recipe.place+'</span></span></span><div class="bg-image" style="background-image: url('+recipe.image+');"></div></a>');
