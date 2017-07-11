ac6f47fb4f6d045b493b1d9763e6120916cc04e2
JosefaHerrera <josefa@linuxero.cl>
2017-07-10 20:23:57 -0400

    Imprime un mensaje en la consola cuando el documento HTML este listo

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -3,3 +3,8 @@
 $("#hide").ready(function(){
         $(".js-menu").hide();
     });
+
+//Imprime un mensaje en la consola cuando el documento HTML este listo
+$(document).ready(function(){
+	console.log("pagina cargada");
+});
